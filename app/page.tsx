1e455e98d01bd954f13425f4913c31659b4d4a5c
covendev <namita1698@gmail.com>
2024-02-07 01:32:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const frameMetadata = getFrameMetadata({
       {label: 'Click to unlock', action: 'post_redirect'}
   ],
   image: 'https://cdn.tokenproof.xyz/assets/36be39d9-b1d8-4fc1-8278-20b77cc9ab72/event_page_banner/3c4e536e-2b0b-494a-855d-3f9407facb7b.png',
-  post_url: 'https://magnificent-panda-f599db.netlify.app/api/frame',
+  post_url: 'https://shefi-ethdenver-tix.netlify.app/api/frame',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <img src={'/video.png'}/>
+      <img src={'https://cdn.tokenproof.xyz/assets/36be39d9-b1d8-4fc1-8278-20b77cc9ab72/event_page_banner/3c4e536e-2b0b-494a-855d-3f9407facb7b.png'}/>
     </>
   );
 }
