d1a9ce355b027a2b54ed2d7762613d5c0c7e4813
covendev <namita1698@gmail.com>
2024-02-07 01:48:53 +0530

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect('https://magnificent-panda-f599db.netlify.app/redirect', {status: 302});
+  return NextResponse.redirect('https://folklore-frame.netlify.app/redirect', {status: 302});
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
