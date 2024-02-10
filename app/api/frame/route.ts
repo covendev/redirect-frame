1295ae35cd32f99ffeaccf23dc364201ac4c273f
covendev <namita1698@gmail.com>
2024-02-10 17:38:58 +0530

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect('https://shefi-s10--shefis10-registration.netlify.app/redirect', {status: 302});
+  return NextResponse.redirect('https://shefis10-registration.netlify.app/redirect', {status: 302});
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
