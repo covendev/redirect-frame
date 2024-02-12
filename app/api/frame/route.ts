ef54fbcbd7ebc98bf8247103ce7e0b76b3346087
covendev <namita1698@gmail.com>
2024-02-12 23:56:54 +0530

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  return NextResponse.redirect('https://folklore-frame.netlify.app/redirect', {status: 302});
+  return NextResponse.redirect('https://shefivalentines.netlify.app/redirect', {status: 302});
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
