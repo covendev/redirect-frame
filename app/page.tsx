83e52d945ae6b48e7b7a198776bf102f6e976b06
covendev <namita1698@gmail.com>
2024-02-12 23:29:20 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,34 @@
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
-const frameMetadata = getFrameMetadata({
-  buttons: [
-      {label: 'Join the Lore!', action: 'post_redirect'}
-  ],
-  image: 'https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png',
-  post_url: 'https://folklore-frame.netlify.app/api/frame',
-});
+export default function Page() {
+  const frameMetadata = getFrameMetadata({
+    buttons: [
+      { label: 'Button 1', action: 'https://example.com/1' },
+      { label: 'Button 2', action: 'https://example.com/2' },
+      { label: 'Button 3', action: 'https://example.com/3' },
+      { label: 'Button 4', action: 'https://example.com/4' }
+    ],
+    image: 'https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png',
+    post_url: 'https://folklore-frame.netlify.app/api/frame',
+  });
 
-export const metadata: Metadata = {
-  title: 'Redirect frame',
-  description: 'LFG',
-  openGraph: {
+  const metadata: Metadata = {
     title: 'Redirect frame',
     description: 'LFG',
-    images: ['https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png'],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+    openGraph: {
+      title: 'Redirect frame',
+      description: 'LFG',
+      images: ['https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png'],
+    },
+    other: {
+      ...frameMetadata,
+    },
+  };
 
-export default function Page() {
   return (
     <>
-      <img src={'https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png'}/>
+      <img src={frameMetadata.image} />
     </>
   );
 }
