a3a280d3eca99aefd3da1ffef145f7900165645a
covendev <namita1698@gmail.com>
2024-02-13 23:57:08 +0530

    Update page.tsx

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -11,16 +11,16 @@ export default function RedirectPage() {
 
     // Perform the redirect based on the action
     switch (action) {
-      case 'Learn':
+      case 'learn_redirect':
         window.location.href = 'https://folklore.institute';
         break;
-      case 'RSS':
+      case 'rss_redirect':
         window.location.href = 'https://entries.folklore.institute';
         break;
-      case 'Telegram':
+      case 'telegram_redirect':
         window.location.href = 'https://t.me/folklore_0x';
         break;
-      case 'Membership':
+      case 'membership_redirect':
         window.location.href = ' https://zora.co/collect/oeth:0x2451761fed91d394ad6ba01215f8b46abe70fc96';
         break;
 
