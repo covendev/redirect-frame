43db288ad85bfd37cd11b58d253bd73166515efa
covendev <namita1698@gmail.com>
2024-02-13 22:06:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,34 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-
-interface RedirectPageProps {
-  redirectUrl: string;
-}
-
-export default function RedirectPage({ redirectUrl }: RedirectPageProps) {
-    const router = useRouter();
-
-    useEffect(() => {
-        if (redirectUrl) {
-            // Perform the redirect
-            router.push(redirectUrl); // For client-side redirect
-        }
-    }, [redirectUrl, router]);
-
-    return (
-        <div>
-            <p>Redirecting...</p>
-        </div>
-    );
-}
-
 import { getFrameMetadata } from '@coinbase/onchainkit';
 import type { Metadata } from 'next';
 
-export default function Page() {
-  const handleButtonClick = (url: string) => {
-    // Redirect to the specified URL
-    router.push(url);
-  };
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    { label: 'Button 1', action: 'post_redirect_1' },
+    { label: 'Button 2', action: 'post_redirect_2' },
+    { label: 'Button 3', action: 'post_redirect_3' },
+    { label: 'Button 4', action: 'post_redirect_4' }
+  ],
+  image: 'https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png',
+  post_url: 'https://folklore-frame.netlify.app/api/frame',
+});
 
-  const frameMetadata = getFrameMetadata({
-    buttons: [
-      { label: 'Learn', action: 'https://folklore.insitute' },
-      { label: 'RSS', action: 'entries.folklore.institue' },
-      { label: 'Join Telegram', action: 'https://t.me/folklore_0x' },
-      { label: 'Membership', action: 'https://zora.co/collect/oeth:0x2451761fed91d394ad6ba01215f8b46abe70fc96' }
-    ],
-    image: 'https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png',
-    post_url: 'https://folklore-frame.netlify.app/api/frame',
-  });
-
-  const metadata: Metadata = {
+export const metadata: Metadata = {
+  title: 'Redirect frame',
+  description: 'LFG',
+  openGraph: {
     title: 'Redirect frame',
     description: 'LFG',
-    openGraph: {
-      title: 'Redirect frame',
-      description: 'LFG',
-      images: ['https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png'],
-    },
-    other: {
-      ...frameMetadata,
-    },
-  };
+    images: ['https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png'],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
 
+export default function Page() {
   return (
     <>
-      <img src={frameMetadata.image} />
-      {/* Display buttons */}
-      {frameMetadata.buttons.map((button, index) => (
-        <button key={index} onClick={() => handleButtonClick(button.action)}>
-          {button.label}
-        </button>
-      ))}
+      <img src={'https://images.mirror-media.xyz/publication-images/yvayTwllxkEx4rX63mYhO.png'}/>
     </>
   );
 }
