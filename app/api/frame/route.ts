798718d4e6abc4fb39293b799047af9b76561b14
covendev <namita1698@gmail.com>
2024-02-13 23:40:47 +0530

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,34 +1,7 @@
-
-
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // Extract data from the request body to determine which redirect to perform
-  const { action } = await req.body.json();
-
-  // Determine the redirect URL based on the action
-  let redirectUrl = 'https://folkloreinstitute.netlify.app/redirect'; // Default redirect URL
-
-  switch (action) {
-      case 'Learn':
-        window.location.href = 'https://folklore.institute';
-        break;
-      case 'RSS':
-        window.location.href = 'entries.folklore.institute';
-        break;
-      case 'Telegram':
-        window.location.href = 'https://t.me/folklore_0x';
-        break;
-      case 'Membership':
-        window.location.href = ' https://zora.co/collect/oeth:0x2451761fed91d394ad6ba01215f8b46abe70fc96';
-        break;
-    default:
-      // Handle default case if action is not recognized
-      break;
-  }
-
-  // Perform the redirect
-  return NextResponse.redirect(redirectUrl, { status: 302 });
+  return NextResponse.redirect('https://folklore-frame.netlify.app/redirect', {status: 302});
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
@@ -36,4 +9,3 @@ export async function POST(req: NextRequest): Promise<Response> {
 }
 
 export const dynamic = 'force-dynamic';
-
